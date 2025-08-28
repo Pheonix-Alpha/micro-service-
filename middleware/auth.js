c89dfb613137c949291cbab292b18a8ddd2c59c6
Pheonix-Alpha <yrohan142@gmail.com>
2025-08-28 12:52:14 +0530

    Header fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export default function authMiddleware(req, res ,next){
    
-    const authHeader = req.headers("authorization");
+    const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
     if(!token) return res.status(401).json({msg:"no token available"});
