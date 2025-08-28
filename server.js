d73814cff107d4a2a6ce3e527994ddfe610f8b4f
Pheonix-Alpha <yrohan142@gmail.com>
2025-08-28 14:01:56 +0530

    backend cart fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: ["https://magnificent-sherbet-8b5e97.netlify.app","http://localhost:5173"
   ],
   credentials: true,
-   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allow these methods
+ methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"], 
 }));
 
